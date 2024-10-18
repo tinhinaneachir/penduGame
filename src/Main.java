ba28809b971fcb3e6c95c419a63a4fb0312a2d8b
tina <tinhinaneachir@gmail.com>
2024-10-18 13:56:25 +0200

    First commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        var saisieUtilisateurs = sc.nextLine();
+
+        while (true){
+
+        }
+
+
+    }
+}
