3b99289a176e11b3094ff16b8808c2ac00854c35
tina <tinhinaneachir@gmail.com>
2025-01-20 16:20:38 +0100

    implementation de la class GuessGame

--- src/GameWord.java
+++ src/GameWord.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameWord {
+
+
+    private List<Character> secretWorl = new ArrayList<>();
+    private int lifePoint;
+    private List<Character> guessWorl = new ArrayList<>();
+
+    public GuessGame(String secretWorl, int lifePoint){
+        for(char c : secretWorl.toCharArray()){
+            this.secretWorl.add(c);//ajout du caracter à chaque entréee utilisateur
+        }
+        this.lifePoint = lifePoint;
+        for(int index; index< secretWorl.length(); index++){
+            this.guessWorl.add('_');
+        }
+    }
+
+    @Override
+    public String toString(){
+        return "mot à deviner"+guessWorl+
+                "| point de vie"+lifePoint;
+    }
+
 }
